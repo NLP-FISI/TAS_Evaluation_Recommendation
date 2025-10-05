1b8c21675593a192a0f0a784c52d9aaa29aced50
FiorellaMirano13 <fiorella.mirano@unmsm.edu.pe>
2025-10-04 22:31:01 -0500

    Agregando router de evaluation performance

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,7 @@
 # This module aggregates all API routes for version 1 of the API.
 from fastapi import APIRouter
-from . import evaluation_input
+from . import evaluation_input, evaluation_performance
 
 router = APIRouter()
 router.include_router(evaluation_input.router)
+router.include_router(evaluation_performance.router)

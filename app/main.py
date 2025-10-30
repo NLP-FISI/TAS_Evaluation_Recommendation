d7716604c8dc541b6a81df26d3cc298a7e2a0bda
Max <max.bruno.saavedra.2002@gmail.com>
2025-10-30 18:45:23 -0500

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-# Main application file
+# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import router as api_router
 
 app = FastAPI(
@@ -8,11 +9,19 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Permitir cualquier conexión (CORS abierto)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # Permite todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],        # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],        # Permite cualquier cabecera
+)
+
 # Rutas principales
 app.include_router(api_router, prefix="/api/v1")
+
 
 @app.get("/")
 def read_root():
     return {"mensaje": "Bienvenido a la API"}
-
-# Run: uvicorn app.main:app --reload
